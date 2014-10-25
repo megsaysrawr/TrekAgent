00d35c9bb138468e87d3a8571172e641926482c5
Xinyu Yi <xinyuyi93@gmail.com>
2014-10-25 07:25:04 -0700

    added MyWishClass

--- gen/com/example/trekagent/R.java
+++ gen/com/example/trekagent/R.java
@@ -23,32 +23,43 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int name_logo=0x7f020001;
     }
     public static final class id {
-        public static final int CurrentLocation=0x7f080001;
-        public static final int MyWishList=0x7f080002;
-        public static final int Search=0x7f080003;
-        public static final int action_settings=0x7f080005;
-        public static final int container=0x7f080004;
+        public static final int CurrentLocation=0x7f080002;
+        public static final int HongKong=0x7f080007;
+        public static final int LosAngeles=0x7f080006;
+        public static final int MyWishList=0x7f080003;
+        public static final int Paris=0x7f080008;
+        public static final int Search=0x7f080004;
+        public static final int action_settings=0x7f080009;
+        public static final int container=0x7f080005;
+        public static final int imageView=0x7f080001;
         public static final int title=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_map=0x7f030001;
-        public static final int fragment_map=0x7f030002;
+        public static final int activity_my_wish_list=0x7f030002;
+        public static final int fragment_map=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int my_wish_list=0x7f070001;
     }
     public static final class string {
         public static final int CurrentLocation=0x7f050003;
+        public static final int HongKong=0x7f05000a;
+        public static final int LosAngeles=0x7f050009;
         public static final int MyWishList=0x7f050004;
+        public static final int Paris=0x7f05000b;
         public static final int Search=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050007;
         public static final int title=0x7f050002;
         public static final int title_activity_map=0x7f050006;
+        public static final int title_activity_my_wish_list=0x7f050008;
     }
     public static final class style {
         /** 
--- src/com/example/trekagent/MainActivity.java
+++ src/com/example/trekagent/MainActivity.java
@@ -2,45 +2,58 @@
 package com.example.trekagent;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.content.Context;
  
 public class MainActivity extends Activity {
 /** Called when the activity is first created. */
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-     
-    try {
-    View.OnClickListener handler = new View.OnClickListener(){
-        public void onClick(View v) {
-            //we will use switch statement and just
-            //get thebutton's id to make things easier
-            switch (v.getId()) {
- 
-                case R.id.CurrentLocation:
-                    Toast.makeText(getBaseContext(), "You Clicked Current Location!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.MyWishList:
-                	Toast.makeText(getBaseContext(),  "You Clicked My Wish List!", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.Search:
-                	Toast.makeText(getBaseContext(),  "You Clicked Search!", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-        }
-    };
-         
-    //we will set the listeners
-    findViewById(R.id.CurrentLocation).setOnClickListener(handler);
-    findViewById(R.id.MyWishList).setOnClickListener(handler);
-    findViewById(R.id.Search).setOnClickListener(handler);
-     
-    }catch(Exception e){
-         Log.e("ERROR", e.toString());
-    } 
-}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+    	final Context context = this;
+	    try {
+	    View.OnClickListener handler = new View.OnClickListener(){
+	        public void onClick(View v) {
+	            //we will use switch statement and just
+	            //get thebutton's id to make things easier
+
+	            switch (v.getId()) {
+	                case R.id.CurrentLocation:
+	                    Toast.makeText(getBaseContext(), "You Clicked Current Location!", Toast.LENGTH_SHORT).show();
+	                    break;
+	                case R.id.MyWishList:
+	                	Toast.makeText(getBaseContext(),  "You Clicked My Wish List!", Toast.LENGTH_SHORT).show();
+	                    Intent intent = new Intent(context, MyWishList.class);
+	                	startActivity(intent);
+	                	break;
+	                case R.id.Search:
+	                	Toast.makeText(getBaseContext(),  "You Clicked Search!", Toast.LENGTH_SHORT).show();
+	                    break;
+	            }
+	        }
+	        
+	    };
+	         
+	    //we will set the listeners
+	    findViewById(R.id.CurrentLocation).setOnClickListener(handler);
+	    findViewById(R.id.MyWishList).setOnClickListener(handler);
+	    findViewById(R.id.Search).setOnClickListener(handler);
+	     
+	    }catch(Exception e){
+	         Log.e("ERROR", e.toString());
+	    } 
+	}
+	
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
 }
